c640bbefc40fa44904bc9994879abe5a24e1d403
LevanceWam <lkwamley@fullsail.edu>
2016-09-29 23:00:56 -0400

    Finished commenting throughout the code going to work on readme

--- src/createURL.js
+++ src/createURL.js
@@ -1,4 +1,7 @@
 exports.shortURL = (link) => {
+  // I am running a math.random to generate a random url for vance.com
   let URLId = 'http://vance.com/' + Math.random().toString(32.5).slice(2);
+
+  // I will return the old link and the new shortened link
   return {oldLink : link, sURL: URLId};
 };
--- src/routes/api.js
+++ src/routes/api.js
@@ -1,7 +1,10 @@
+// setting my constant to the createURL.js
 const generateLink = require('../createURL');
+
 module.exports = (express) => {
   const router = express.Router();
   router.post('/url', (req, res) => {
+    // the shorten url will come from a provided link
     res.json(generateLink.shortURL(req.body.link));
   });
   return router;
