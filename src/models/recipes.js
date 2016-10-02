998f549d97bd06cf2ec1b4d544a41a304ae9cc7c
LevanceWam <lkwamley@fullsail.edu>
2016-10-01 23:39:28 -0400

    I finished the recipes.js inside of the models folder

--- src/models/recipes.js
+++ src/models/recipes.js
@@ -1,7 +1,58 @@
 const db = require('./db');
-// ________________________________
+
 
 // Creating the recipe
 exports.add = (data, err, success) => {
   db.recipe.create(data).then(success).catch(err);
+  console.log('This recipe was created from the models');
+};
+
+// __________________________________________________________
+
+// updating the recipe
+exports.update = (data, err, success) => {
+  db.recipe.find({
+    where: {
+      id: data.id,
+    },
+  }).then((existingData) => {
+    existingData.updateAttributes(data).then(success).catch(err);
+  }).catch(err);
+  console.log('This recipe was updated from the models');
+};
+
+// ___________________________________________________________
+
+// deleting the recipe
+exports.remove = (data, err, success) => {
+  db.recipe.destory({
+    where: {
+      id: data.id,
+    },
+  }).then(success).catch(err);
+  console.log('This recipe was deleted from the models');
+};
+
+// ___________________________________________________________
+
+// finding one of the recipes
+exports.one = (data, err, success) => {
+  db.recipe.find({
+    where: {
+      id: data.id,
+    },
+    include: [{
+      all: true,
+      nested: true,
+    }],
+  }).then(success).catch(err);
+  console.log('This one recipe was found from the models');
+};
+
+// ___________________________________________________________
+
+// finding all of the recipes
+exports.all = (err, success) => {
+  db.recipe.findAll().then(success).catch(err);
+  console.log('These recipes were found from the models');
 };
--- src/routes/api/recipes.js
+++ src/routes/api/recipes.js
@@ -13,6 +13,65 @@ module.exports = (express) => {
     });
   });
 
+// ___________________________________________________________
+
+
+// Find one recipe
+router.post('/url', (req, res) => {
+  recipes.add(req.body, (err) => {
+    res.status(500).json(err);
+  }, (data) => {
+    res.status(200).json(data);
+  });
+});
+
+// ___________________________________________________________
+
+
+// Find All recipes
+router.post('/url', (req, res) => {
+  recipes.add(req.body, (err) => {
+    res.status(500).json(err);
+  }, (data) => {
+    res.status(200).json(data);
+  });
+});
+
+// ___________________________________________________________
+
+
+// Delete recipes
+router.post('/url', (req, res) => {
+  recipes.add(req.body, (err) => {
+    res.status(500).json(err);
+  }, (data) => {
+    res.status(200).json(data);
+  });
+});
+
+// ___________________________________________________________
+
+// Update recipes
+router.post('/url', (req, res) => {
+  recipes.add(req.body, (err) => {
+    res.status(500).json(err);
+  }, (data) => {
+    res.status(200).json(data);
+  });
+});
+
+// ___________________________________________________________
+
+// Create recipes
+router.post('/url', (req, res) => {
+  recipes.add(req.body, (err) => {
+    res.status(500).json(err);
+  }, (data) => {
+    res.status(200).json(data);
+  });
+});
+
+// ___________________________________________________________
 
   return router;
 };
